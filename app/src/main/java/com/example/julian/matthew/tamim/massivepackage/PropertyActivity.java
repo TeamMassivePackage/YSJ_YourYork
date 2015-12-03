daa18226495797644ecedac9a8ed35784757ec1a
julianmoses <jmoses2@gmail.com>
2015-12-03 22:47:10 +0000

    Added Spinners to property activity.
    Added Spinner adapters to property activity.
    
    Added string arrays to string.xml file for the text that is in each spinner

--- app/src/main/java/com/example/julian/matthew/tamim/massivepackage/PropertyActivity.java
+++ app/src/main/java/com/example/julian/matthew/tamim/massivepackage/PropertyActivity.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class PropertyActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -32,6 +34,42 @@ public class PropertyActivity extends AppCompatActivity implements NavigationVie
         //SET UP NAVIGATION DRAWER AND DRAWER ITEM SELECTED LISTENER
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view_property);
         navigationView.setNavigationItemSelectedListener(this);
+
+        Spinner radius_spinner = (Spinner)findViewById(R.id.spinner_radius);
+        ArrayAdapter<CharSequence> radiusAdapter = ArrayAdapter.createFromResource(this,
+                R.array.radius_array, android.R.layout.simple_spinner_item);
+        radiusAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        radius_spinner.setAdapter(radiusAdapter);
+
+        Spinner bedMin_spinner = (Spinner)findViewById(R.id.spinner_beds_min);
+        ArrayAdapter<CharSequence> bedsMinAdapter = ArrayAdapter.createFromResource(this,
+                R.array.bedsMin_array, android.R.layout.simple_spinner_item);
+        bedsMinAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        bedMin_spinner.setAdapter(bedsMinAdapter);
+
+        Spinner bedMax_spinner = (Spinner)findViewById(R.id.spinner_beds_max);
+        ArrayAdapter<CharSequence> bedsMaxAdapter = ArrayAdapter.createFromResource(this,
+                R.array.bedsMax_array, android.R.layout.simple_spinner_item);
+        bedsMaxAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        bedMax_spinner.setAdapter(bedsMaxAdapter);
+
+        Spinner priceMin_spinner = (Spinner)findViewById(R.id.spinner_price_min);
+        ArrayAdapter<CharSequence> priceMinAdapter = ArrayAdapter.createFromResource(this,
+                R.array.priceMin_array, android.R.layout.simple_spinner_item);
+        priceMinAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        priceMin_spinner.setAdapter(priceMinAdapter);
+
+        Spinner priceMax_spinner = (Spinner)findViewById(R.id.spinner_price_max);
+        ArrayAdapter<CharSequence> priceMaxAdapter = ArrayAdapter.createFromResource(this,
+                R.array.priceMax_array, android.R.layout.simple_spinner_item);
+        priceMaxAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        priceMax_spinner.setAdapter(priceMaxAdapter);
+
+        Spinner added_spinner = (Spinner)findViewById(R.id.spinner_added);
+        ArrayAdapter<CharSequence> addedAdapter = ArrayAdapter.createFromResource(this,
+                R.array.added_array, android.R.layout.simple_spinner_item);
+        addedAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        added_spinner.setAdapter(addedAdapter);
     }
 
     @Override
